9e8f586987918ff284c96c6e5a71eb974a0c45dc
Francisco Ramirez <franmaster0@gmail.com>
2022-02-07 18:32:43 -0300

    Space Turism Challenge

---
+++ code-base/script.js
@@ -0,0 +1,25 @@
+const navButton = document.getElementById("open-nav");
+const navBar = document.getElementById("page-header");
+const closeButton = document.getElementById("close-nav");
+navButton.addEventListener("click", () => {
+    if (navBar.style.display !== "none") {
+        navBar.style.display = "none";
+        navButton.style.visibility = "visible";
+        closeButton.style.visibility = "hidden";
+    }else {
+        navBar.style.display = "flex";
+        navButton.style.visibility = "hidden";
+        closeButton.style.visibility = "initial";
+    };
+});
+closeButton.addEventListener("click", () => {
+    if (navBar.style.display !== "none") {
+        navBar.style.display = "none";
+        navButton.style.visibility = "visible";
+        closeButton.style.visibility = "hidden";
+    }else {
+        navBar.style.display = "flex";
+        navButton.style.visibility = "hidden";
+        closeButton.style.visibility = "initial";
+    };
+});
